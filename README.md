64d351a27e76844de5b71d47025797c98f7c65d3
Fraenz Friederes <fraenz@frieder.es>
2014-10-01 02:43:10 +0200

    Updated to version 0.0.3.

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 dioder
 ======
 
-A node package that gives your Raspberry Pi control over your IKEA DIODER LED light strip.
+A node package that gives your Raspberry Pi control over your Ikea Dioder LED light strip.
 
 ## Install
 
-Make sure you have `node` and `npm` installed on your Raspberry Pi.
+Connect your Dioder LED light strip to your Raspberry Pi ([instructions](http://krizzblog.de/2013/12/the-pidioder/)).
 
-Install the [pi-blaster daemon](https://github.com/sarfata/pi-blaster).
+Install the pi-blaster daemon ([instructions](https://github.com/sarfata/pi-blaster)).
+
+Make sure you have `node` and `npm` installed on your Raspberry Pi.
 
 Finally install `dioder`:
 
-    npm install dioder
+    $ npm install dioder
 
 
 ## Usage
 
-### Constructor
+### Instance
 
 Creates a dioder object. You don't need to provide any options, these are the defaults.
 
@@ -26,42 +28,99 @@ var Color = require('color');
 
 var dioder = Dioder({
     color: Color('#ffffff'),
-    easing: function(t, b, c, d) { ... },
+    defaultEasing: 'easeInOutQuad',
     colorComponentPins: [4, 17, 18],
-    animationInterval: 25
+    animationInterval: 25,
+    idleCallback: null,
+    loopQueue: false
 });
 ```
 
-### changeTo
+### Color
+
+Color values are expected to be created by the [harthur/color](https://github.com/harthur/color) library. Only RGB values (no alpha) are supported.
+
+```javascript
+var color = Color('#ffffff');
+var color = Color('rgb(255, 255, 255)');
+var color = Color().rgb(255, 255, 255);
+var color = Color().rgb([255, 255, 255]);
+```
+
+
+### Easing
+
+Easing values are expected to be a `function` or one of the following keys: `linear`, `easeInQuad`, `easeOutQuad`, `easeInOutQuad`, `easeInCubic`, `easeOutCubic`, `easeInOutCubic`, `easeInQuart`, `easeOutQuart`, `easeInOutQuart`, `easeInQuint`, `easeOutQuint`, `easeInOutQuint`.
+
+Easing expressed by a function:
+
+```javascript
+// expressed by a function
+var easing = function(t) {
+    return t<.5 ? 2*t*t : -1+(4-2*t)*t
+}
+
+// expressed by a key
+var easing = 'easeInOutQuad';
+```
+
+
+### Methods
+
+#### getColor
+
+Returns the current color.
+
+```javascript
+dioder.getColor();
+```
+
+#### getLastQueuedColor
+
+Returns the ending color of the animation queue. If `loopQueue` is set to `true`, this value changes during animation.
+
+```javascript
+dioder.getLastQueuedColor();
+```
+
+#### changeTo
 
 Changes the color without transition. Calls the callback when completed.
 
 ```javascript
-changeTo(color, [callback]);
+dioder.changeTo(color, [callback]);
 ```
 
-### animateTo
+#### animateTo
 
-Animates from the current color to a specified one in a given duration and easing. Calls the callback when completed.
+Animates from the previous color (obtained by `getLastQueuedColor`) to a specified one in a given duration and easing. Calls the callback when completed.
 
 ```javascript
-animateTo(color, transitionDuration, [callback], [easing]);
+dioder.animateTo(color, [duration], [easing], [callback]);
 ```
 
-### delay
+#### delay
 
-Delays the next queued animations by a given duration.
+Delays the next queued animations by a given duration. Calls the callback when completed.
 
 ```javascript
-delay(duration);
+dioder.delay(duration, [callback]);
 ```
 
-### stop
+#### stop
 
-Interrupts the current transition and clears the queue. In this case, no callbacks (sent to `changeTo` or `animateTo`) are called.
+Interrupts the current transition and clears the queue. In this case, no callbacks (sent to `changeTo`, `animateTo` or `delay`) are called.
 
 ```javascript
-stop();
+dioder.stop();
+```
+
+#### setLoopQueue
+
+If set to `true` completed animations will be readded to the end of the animation queue.
+
+```javascript
+dioder.setLoopQueue(loopQueue);
 ```
 
 ## Examples
@@ -78,21 +137,41 @@ dioder
 ### Red Alert
 
 ```javascript
-function animateToRed() {
-    dioder.animateTo(Color('#ff0000'), 1500, animateToBlack);
+dioder
+    .stop()
+    .setLoopQueue(true)
+    .animateTo(Color('#ff0000'), 1500)
+    .animateTo(Color('#300000'), 1500);
+```
+
+### Easing
+
+```javascript
+var firstColor = Color('#ffffff');
+var secondColor = Color('#ff0000');
+
+var dioder = Dioder({
+    color: firstColor
+});
+
+var easingFunctions = [
+    'linear',
+    'easeInQuad', 'easeOutQuad', 'easeInOutQuad',
+    'easeInCubic', 'easeOutCubic', 'easeInOutCubic',
+    'easeInQuart', 'easeOutQuart', 'easeInOutQuart',
+    'easeInQuint', 'easeOutQuint', 'easeInOutQuint'
+];
+
+for (var i = 0; i < easingFunctions.length; i ++)
+{
+    var easing = easingFunctions[i];
+
+    // wait for 1 second and log the easing method
+    dioder.delay(1000, function() { console.log('' + this); }.bind(easing));
+    
+    // animate to the other color using easing
+    dioder.animateTo((i % 2 !== 0 ? firstColor : secondColor), 2000, easing);
 }
-
-function animateToBlack() {
-    dioder.animateTo(Color('#300000'), 1500, animateToRed);
-}
-
-// start animation
-animateToBlack();
-
-// stop animation after 30 seconds
-setTimeout(function() {
-    dioder.stop();
-}, 30000);
 ```
 
 
--- dioder.js
+++ dioder.js
@@ -6,6 +6,22 @@
 	var piblaster = require('pi-blaster.js');
 	var Color = require('color');
 
+	var EASING_FUNCTIONS = {
+		linear: function(t) { return t },
+		easeInQuad: function(t) { return t*t },
+		easeOutQuad: function(t) { return t*(2-t) },
+		easeInOutQuad: function(t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+		easeInCubic: function(t) { return t*t*t },
+		easeOutCubic: function(t) { return (--t)*t*t+1 },
+		easeInOutCubic: function(t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+		easeInQuart: function(t) { return t*t*t*t },
+		easeOutQuart: function(t) { return 1-(--t)*t*t*t },
+		easeInOutQuart: function(t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+		easeInQuint: function(t) { return t*t*t*t*t },
+		easeOutQuint: function(t) { return 1+(--t)*t*t*t*t },
+		easeInOutQuint: function(t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+	};
+
 	module.exports = function(options)
 	{
 		return new Dioder(options);
@@ -15,12 +31,15 @@
 	{
 		// attributes
 		this._color = null;
-		this._easing = null;
+		this._defaultEasing = null;
 		this._colorComponentPins = null;
 
 		this._animationQueue = [];
 		this._animationLoopInterval = null;
 		this._animationLoopTickDuration = null;
+		this._loopAnimationQueue = false;
+
+		this._idleCallback = null;
 
 		// init
 		this.init(options);
@@ -32,42 +51,36 @@
 
 		// read options
 		var color = options.color || Color('#000000');
-		this._easing = options.easing || this.easeInOutQuad;
+		this._defaultEasing = options.defaultEasing || 'easeInOutQuad';
 		this._colorComponentPins = options.colorComponentPins || [4, 17, 18];
 		this._animationLoopTickDuration = options.animationInterval || 25;
+		this._idleCallback = options.idleCallback || null;
+		this._loopAnimationQueue = options.loopQueue || false;
 
 		// apply initial color
 		this._applyColor(color);
 	};
 
-	Dioder.prototype.easeInOutQuad = function(t, b, c, d)
+	Dioder.prototype.animateTo = function(color, duration, easing, callback)
 	{
-		t /= d / 2;
-		if (t < 1)
-			return c / 2 * t * t + b;
-		t--;
-		return -c / 2 * (t * (t - 2) - 1) + b;
-	};
-
-	Dioder.prototype.animateTo = function(color, transitionDuration, callback, easing)
-	{
-		var from = this._getLastQueuedColor();
+		var from = this.getLastQueuedColor();
 
 		// queue animation step
 		this._animationQueue.push({
 			from: from,
 			to: color,
 			time: 0,
-			duration: transitionDuration,
-			easing: easing ? easing : this._easing,
-			callback: callback ? callback : null
+			duration: (duration !== undefined ? duration : 1000),
+			easing: easing || this._defaultEasing,
+			callback: callback || null
 		});
 
 		if (this._animationLoopInterval === null)
 		{
 			// turn on animation loop
 			this._animationLoopInterval = setInterval(
-				this._animationLoop.bind(this), this._animationLoopTickDuration);
+				this._animationLoop.bind(this),
+				this._animationLoopTickDuration);
 		}
 
 		return this;
@@ -75,13 +88,13 @@
 
 	Dioder.prototype.changeTo = function(color, callback)
 	{
-		return this.animateTo(color, 0, callback);
+		return this.animateTo(color, 0, 'linear', callback);
 	};
 
-	Dioder.prototype.delay = function(delay)
+	Dioder.prototype.delay = function(delay, callback)
 	{
 		// animate to the same color in delay
-		return this.animateTo(this._getLastQueuedColor(), delay, null);
+		return this.animateTo(this.getLastQueuedColor(), delay, 'linear', callback);
 	};
 
 	Dioder.prototype.stop = function()
@@ -95,7 +108,125 @@
 		return this;
 	};
 
-	Dioder.prototype._getLastQueuedColor = function()
+	Dioder.prototype.setLoopQueue = function(loopQueue)
+	{
+		this._loopAnimationQueue = loopQueue;
+		return this;
+	};
+
+	Dioder.prototype._animationLoop = function()
+	{
+		var time = this._animationLoopTickDuration;
+
+		// go through queue to find the current entry
+		var currentEntry = null;
+		while (currentEntry === null && this._animationQueue.length > 0)
+		{
+			// get current stack entry
+			var currentEntry = this._animationQueue[0];
+
+			// add up time
+			currentEntry.time += time;
+
+			// calculate how much time is left
+			time = Math.max(currentEntry.time - currentEntry.duration, 0);
+
+			// check if this entry has been completed
+			if (currentEntry.time >= currentEntry.duration)
+			{
+				// remove entry from queue
+				this._animationQueue.splice(0, 1);
+
+				// call the callback if available
+				if (currentEntry.callback !== null)
+				{
+					currentEntry.callback(this);
+				}
+
+				if (this._loopAnimationQueue)
+				{
+					// reset and requeue this entry
+					currentEntry.time = 0;
+					currentEntry.from = this.getLastQueuedColor();
+					this._animationQueue.push(currentEntry);
+				}
+
+				// apply the entry's end color if it is the last one
+				if (this._animationQueue.length === 1)
+				{
+					this._applyColor(currentEntry.to);
+				}
+				
+				// this animation step is completed
+				currentEntry = null;
+			}
+		}
+
+		if (currentEntry !== null)
+		{
+			// we are inside an animation step
+			// calculate intermediate color
+			var fromColorComponents = currentEntry.from.rgbArray();
+			var intermediateColorComponents = [0, 0, 0];
+			var toColorComponents = currentEntry.to.rgbArray();
+
+			// ease time
+			var easedTime = this._easeTime(
+				currentEntry.time / currentEntry.duration,
+				currentEntry.easing);
+
+			// go through components
+			for (var i = 0; i < 3; i ++)
+			{
+				// calculate intermediate component value
+				intermediateColorComponents[i] =
+					(1 - easedTime) * fromColorComponents[i]
+					+ easedTime * toColorComponents[i];
+			}
+
+			// compose color
+			var intermediateColor = Color().rgb(intermediateColorComponents);
+
+			// apply color
+			this._applyColor(intermediateColor);
+		}
+		else
+		{
+			// turn off animation loop
+			clearInterval(this._animationLoopInterval);
+			this._animationLoopInterval = null;
+
+			// the queue comes to an end
+			if (this._idleCallback !== null)
+			{
+				this._idleCallback(this);
+			}
+		}
+	};
+
+	Dioder.prototype._easeTime = function(time, easing)
+	{
+		if (typeof easing === 'function')
+		{
+			// use given easing function
+			return easing(time);
+		}
+		else if (typeof easing === 'string' && EASING_FUNCTIONS[easing] !== undefined)
+		{
+			// use known easing function
+			return (EASING_FUNCTIONS[easing])(time);
+		}
+
+		// use default easing
+		return EASING_FUNCTIONS['easeInOutQuad'];
+	}
+
+	Dioder.prototype.getColor = function()
+	{
+		return this._color;
+	};
+
+	Dioder.prototype.getLastQueuedColor = function()
 	{
 		var last = this._color;
 
@@ -106,83 +237,7 @@
 		}
 
 		return last;
-	}
-
-	Dioder.prototype._animationLoop = function()
-	{
-		if (this._animationQueue.length > 0)
-		{
-			var time = this._animationLoopTickDuration;
-
-			// go through queue to find the current entry
-			var currentEntry = null;
-			while (time > 0 && this._animationQueue.length > 0)
-			{
-				// get current stack entry
-				var currentEntry = this._animationQueue[0];
-
-				// add up time
-				currentEntry.time += time;
-
-				// calculate how much time is left
-				time = Math.max(currentEntry.time - currentEntry.duration, 0);
-
-				// check if this entry has been completed
-				if (currentEntry.time >= currentEntry.duration)
-				{
-					// apply it's to color if there is no entry following
-					if (this._animationQueue.length === 1)
-					{
-						this._applyColor(currentEntry.to);
-					}
-
-					// remove entry from queue
-					this._animationQueue.splice(0, 1);
-
-					// call the callback if available
-					if (currentEntry.callback !== null)
-					{
-						currentEntry.callback(this);
-					}
-					
-					currentEntry = null;
-				}
-			}
-
-			if (currentEntry !== null)
-			{
-				// we are inside an animation step
-				// calculate intermediate color
-				var fromColorComponents = currentEntry.from.rgbArray();
-				var intermediateColorComponents = [0, 0, 0];
-				var toColorComponents = currentEntry.to.rgbArray();
-
-				// go through components
-				for (var i = 0; i < 3; i ++)
-				{
-					// calculate intermediate component value
-					intermediateColorComponents[i] = currentEntry.easing(
-						currentEntry.time,
-						fromColorComponents[i],
-						toColorComponents[i] - fromColorComponents[i],
-						currentEntry.duration
-					);
-				}
-
-				// compose color
-				var intermediateColor = Color().rgb(intermediateColorComponents);
-
-				// apply color
-				this._applyColor(intermediateColor);
-			}
-		}
-		else
-		{
-			// turn off animation loop
-			clearInterval(this._animationLoopInterval);
-			this._animationLoopInterval = null;
-		}
-	}
+	};
 
 	Dioder.prototype._applyColor = function(color)
 	{
@@ -201,7 +256,7 @@
 			}
 		}
 
-		// keep track of the displayed color
+		// keep track of the current color
 		this._color = color;
 	};
 
